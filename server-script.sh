eb9f4320c270561bf588e4450bba41bad1f65f1a
unknown <swarnavure@gmail.com>
2022-12-02 16:00:25 +0530

    server script file modified

--- server-script.sh
+++ server-script.sh
@@ -4,11 +4,11 @@ sudo yum install maven -y
 if [-d "addressbook"]
 then
   echo "repo is cloned and exists"
-  cd/home/ec2-user/addressbook
+  cd /home/ec2-user/addressbook
   git pull origin master
 else
    echo "repo is not there"
 git clone https://github.com/swarnavure/addressbook.git 
-cd/home/ec2-user/addressbook
+cd /home/ec2-user/addressbook
 fi
 mvn package
