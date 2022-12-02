d619f20e48ad739f5dbd624045172a4fe93b015e
unknown <swarnavure@gmail.com>
2022-12-02 15:54:48 +0530

    server script file added

---
+++ server-script.sh
@@ -0,0 +1,14 @@
+sudo yum install java-1.8.0-openjdk-deve1 -y
+sudo yum install git -y
+sudo yum install maven -y
+if [-d "addressbook"]
+then
+  echo "repo is cloned and exists"
+  cd/home/ec2-user/addressbook
+  git pull origin master
+else
+   echo "repo is not there"
+git clone https://github.com/swarnavure/addressbook.git 
+cd/home/ec2-user/addressbook
+fi
+mvn package
